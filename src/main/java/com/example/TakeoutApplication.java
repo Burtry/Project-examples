a9e6bbb1c6320615100940ca8ef586be5880092d
Burtry <1971797331@qq.com>
2023-10-16 11:59:27 +0800

    Init code

---
+++ src/main/java/com/example/TakeoutApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeoutApplication.class, args);
+	}
+
+}
