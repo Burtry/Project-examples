afb3c77add494300eda414cbeaba738bf0a5ed9d
Burtry <1971797331@qq.com>
2023-10-16 15:00:34 +0800

    modify code

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -11,6 +11,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
-        log.info("server started");
+        log.info("启动!");
     }
 }
