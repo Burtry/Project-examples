282129f44dcc41b3f498861ce87566218a1d717e
Burtry <1971797331@qq.com>
2023-10-16 15:22:03 +0800

    update code

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -12,7 +12,5 @@ public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("启动!!!!");
-        log.info("1111");
-
     }
 }
