77a5c951fa59f5fa23648a428640ecd74deb169d
Burtry <1971797331@qq.com>
2023-11-06 10:25:54 +0800

    update code34---- 历史订单

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -2,6 +2,7 @@ package com.sky.controller.user;
 
 import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
@@ -44,4 +45,17 @@ public class OrderController {
         //log.info("模拟交易成功:{}",ordersPaymentDTO.getOrderNumber());
         return Result.success(orderPaymentVO);
     }
+
+    /**
+     * 历史订单查询
+     * @param
+     * @return
+     */
+    @GetMapping("/historyOrders")
+    public Result<PageResult> page(int page, int pageSize, Integer status) {
+        PageResult pageResult = orderService.page(page,pageSize,status);
+        return Result.success(pageResult);
+    }
+
+
 }
